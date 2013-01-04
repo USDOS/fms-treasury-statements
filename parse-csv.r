5e27818509fc78ec39511e71309d1d994f6fa744
Thomas Levine <occurrence@thomaslevine.com>
2013-01-04 16:01:06 -0500

    parse table 5

--- parse-csv.r
+++ parse-csv.r
@@ -1,15 +1,26 @@
 library(reshape2)
 strip <- function(string) {
-    # Strip leading and trailing spaces
-    str_replace(str_replace(string, '^ *', ''), ' *$', '')
+    # Strip leading dollar sign and leading and trailing spaces
+    str_replace(str_replace(string, '^[$]? *', ''), ' *$', '')
 }
 
-table5 <- read.fwf(
+# Load file
+table5.wide <- read.fwf(
     'archive/12121400/table5.fixie',
     c(40, 12, 1, 12, 1, 12, 1, 13),
     stringsAsFactors = F
 )[-c(2, 7),c(1, 2, 4, 6, 8)]
-rownames(table5) <- 1:nrow(table5)
-colnames(table5) <- c('transaction', 'A', 'B', 'C', 'total')
 
-table5$direction <- c(rep('income', 5), rep('expense', 6))
+# Names
+rownames(table5.wide) <- 1:nrow(table5.wide)
+colnames(table5.wide) <- c('transaction', 'A', 'B', 'C', 'total')
+
+# Remove spaces.
+table5.wide <- data.frame(lapply(table5.wide, strip), stringsAsFactors = F)
+table5.wide[-1] <- data.frame(lapply(table5.wide[-1], as.numeric))
+
+# Income or expense?
+table5.wide$direction <- c(rep('income', 5), rep('expense', 6))
+
+table5.long <- melt(table5.wide, c('direction', 'transaction'),
+    variable.name = 'type.of.depository', value.name = 'amount')
