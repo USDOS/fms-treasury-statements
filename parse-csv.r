9e8c4945d933cb72973c7002a08e4dc5b8f56e28
Thomas Levine <occurrence@thomaslevine.com>
2013-01-04 16:18:46 -0500

    table1

--- parse-csv.r
+++ parse-csv.r
@@ -1,7 +1,37 @@
 library(reshape2)
 strip <- function(string) {
-    # Strip leading dollar sign and leading and trailing spaces
-    str_replace(str_replace(string, '^[$]? *', ''), ' *$', '')
+    # Strip leading and trailing spaces
+    str_replace(str_replace(string, '^ *', ''), ' *$', '')
+}
+
+number <- function(string){
+    # Remove the leading dollar sign, remove commas, and convert to numeric.
+    as.numeric(str_replace_all(str_replace(strip(string), '^[$]?', ''), ',', ''))
+}
+
+#                                                                Opening balance
+#                                          Closing   ______________________________________
+#           Type of account                balance                    This         This
+#                                           today        Today        month       fiscal
+#                                                                                  year
+#___________________________________________________________________________________________
+ 
+
+table1 <- function() {
+    # Load file
+    table1.wide <- read.fwf(
+        'archive/12121400/table1.fixie',
+        c(40, 12, 1, 12, 1, 12, 1, 13),
+        stringsAsFactors = F
+    )[,-c(3, 5, 7)]
+    colnames(table1.wide) <- c('type', 'closing.balance.today', 'today', 'this.month', 'this.fiscal.year')
+    table1.wide[2, 1] <- paste(strip(table1.wide[2:3, 1]), collapse = ' ')
+    table1.wide <- table1.wide[-3,]
+
+    table1.wide[1] <- strip(table1.wide[,1])
+    table1.wide[-1] <- data.frame(lapply(table1.wide[-1], number))
+
+    table1.wide
 }
 
 table5 <- function() {
@@ -17,11 +47,11 @@ table5 <- function() {
     colnames(table5.wide) <- c('transaction', 'A', 'B', 'C', 'total')
     
     # Remove spaces.
-    table5.wide <- data.frame(lapply(table5.wide, strip), stringsAsFactors = F)
-    table5.wide[-1] <- data.frame(lapply(table5.wide[-1], as.numeric))
+    table5.wide[1] <- strip(table5.wide[,1])
+    table5.wide[-1] <- data.frame(lapply(table5.wide[-1], number))
     
     # Income or expense?
-    table5.wide$direction <- c(rep('income', 5), rep('expense', 6))
+    table5.wide$direction <- factor(c(rep('income', 5), rep('expense', 6)))
     
     table5.long <- melt(table5.wide, c('direction', 'transaction'),
         variable.name = 'type.of.depository', value.name = 'amount')
