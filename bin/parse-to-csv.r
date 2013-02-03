4c1e052e4422b954d408f508c2b50caa3bfb3658
Jake Bialer <jbialer@gmail.com>
2013-02-03 13:49:03 -0500

    Parsing for table 3a and 3c. Hacky

--- bin/parse-to-csv.r
+++ bin/parse-to-csv.r
@@ -18,7 +18,11 @@ number <- function(string){
 #                                           today        Today        month       fiscal
 #                                                                                  year
 #___________________________________________________________________________________________
- 
+
+extra_chars <- function(string){
+  string <- str_replace_all(strip(string),'1/', '')
+  string <- str_replace_all(strip(string),'2/', '')
+}
 
 table1 <- function() {
     # Load file
@@ -123,7 +127,70 @@ table2 <- function(datestamp) {
         'is.total', 'today', 'mtd', 'ytd', 'footnotes'
     )]
 }
-
+table3a <- function(datestamp, file){
+  # Load file 
+  alltables<-readLines(file)
+  page4.line <- grep('PAGE',alltables)[4]
+  print(page4.line)
+  page5.line <- grep('PAGE',alltables)[5]
+  print(page5.line)
+  
+  table3.wide <- read.fwf(file, c(40, 12, 1, 12, 1, 12, 1, 13), stringsAsFactors=F,skip=page4.line, n=page5.line-page4.line-1)[,-c(2,3,5,7)]
+  # strip leading and trailing spaces  
+  colnames(table3.wide) <- c('item', 'today', 'mtd', 'ytd')
+  # Remove NA rows
+  table3.wide <- na.omit(table3.wide)
+  
+  
+  regular.series.line <- grep('Regular Series',table3.wide$item)[1]
+  total.issues.line <- grep('Total Issues',table3.wide$item)[1]
+  bills.line <- grep('Bills',table3.wide$item)[1]
+  table3.wide$type <- factor(c(rep('Issues', total.issues.line), rep('Redemptions', nrow(table3.wide)-total.issues.line)))
+  table3.wide <- table3.wide[-c(1:regular.series.line-1,(total.issues.line+1):(bills.line-1)),]
+  table3.wide[1] <- strip(table3.wide[,1])
+  table3.wide$date <- datestamp
+  table3.wide$table <- 3
+  table3.wide$datatype <- ifelse(table3.wide[,'item']=='Total Issues' | table3.wide[,'item']=='Total Redemptions','Total','Other')
+  table3.wide[table3.wide[,'item']=='Net Change in Public Debt Outstanding','datatype'] = 'Net'
+  table3.wide[c('today', 'mtd', 'ytd')] <- data.frame(lapply(table3.wide[c('today', 'mtd', 'ytd')], number))
+  return(table3.wide)
+}
+table3c <- function(datestamp, file){
+  # Load file and find location of table 
+  alltables<-readLines(file)
+  page6.line <- grep('PAGE',alltables)[6]
+  page7.line <- grep('PAGE',alltables)[7]
+  
+  widths <- c(32, 15, 15, 15, 15)
+  # Widths change in 2012 
+  if(datestamp>='2012-06-01'){
+    widths <- c(38, 15, 13,13,13)
+  }
+  
+  # Messy. Two files have spacing issues. 
+  if(datestamp=='2010-01-11' | datestamp=='2010-03-02'){
+    table3.wide <- read.fwf(file, widths, stringsAsFactors=F,skip=page6.line, n=page7.line-page6.line+1,blank.lines.skip=T) 
+  }
+  else{  
+    table3.wide <- read.fwf(file, widths, stringsAsFactors=F,skip=page6.line, n=32,blank.lines.skip=T)
+  }
+  colnames(table3.wide) <- c('balance_trans','closing_bal_today', 'opening_today', 'opening_mtd', 'opening_ytd')
+  
+  #remove NA values 
+  table3.wide <- na.omit(table3.wide)
+  
+  gov.debt.line <- grep('Debt Held',table3.wide$balance_trans)[1]
+  stat.debt.line <- grep('Statutory Debt Limit',table3.wide$balance_trans)[1]
+  table3.wide <- table3.wide[gov.debt.line:stat.debt.line,]
+  
+  # strip leading and trailing spaces  
+  table3.wide[1] <- strip(table3.wide[,1])
+  table3.wide$date <- datestamp
+  table3.wide$table <- 4
+  table3.wide[c('closing_bal_today', 'opening_today', 'opening_mtd','opening_ytd')] <- data.frame(lapply(table3.wide[c('closing_bal_today', 'opening_today', 'opening_mtd','opening_ytd')], number))
+  table3.wide[c('balance_trans')] <- data.frame(lapply(table3.wide[c('balance_trans')],extra_chars))
+  return(table3.wide)
+}
 table5 <- function() {
     # Load file
     table5.wide <- read.fwf(
